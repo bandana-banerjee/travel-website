fd99a56a822807c7b1b41af44b71aaecf06900a8
jamesgade <gadejames48@gmail.com>
2024-01-21 01:07:06 +0530

    carousel height fix

--- src/App.js
+++ src/App.js
@@ -5,25 +5,18 @@ import About from './components/about';
 import Services from './components/services';
 import Tour from './components/tours';
 import Footer from './components/footer';
+
 function App() {
   return (
-  <>
-  
-<Navbar/>
-<Hero/>
-<About/>
-<Services/>
-<Tour/>
-<Footer/>
-
-    
-
-  
-
-    
-
-    
-  </>);
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tour />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/hero.js
+++ src/components/hero.js
@@ -2,35 +2,32 @@
 import Slider from "react-slick";
 import React, { Component } from "react";
 //import { baseUrl } from "./config";
-import {list} from '../data'
+import { list } from '../data'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 function hero() {
   const settings = {
-      dots: true,
-      fade: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-        autoplaySpeed: 1000,
-        pauseOnHover: true,
-    };
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
+  };
   return (
-   <section className="slick-container" id="home">
-        <Slider {...settings}>
-          {list.map((person) => {
-                    const { id, image} = person;
-                    return <div key={id} >
-                        <img src={image} />
-                        
-                        
-                    </div>
-                })}
-        </Slider>
-      </section>
-    
+    <section className="slick-container" id="home">
+      <Slider {...settings}>
+        {list.map((person) => {
+          const { id, image } = person;
+          return <div key={id} >
+            <img src={image} />
+          </div>
+        })}
+      </Slider>
+    </section>
   )
 }
 
