cc9ad56e3c34b5cb9af024c676ae2e4f6ddd3a0d
Bandana Banerjee <bbandana2012@gmail.com>
2024-01-21 23:35:58 +0530

    modified hero section

--- src/data.js
+++ src/data.js
@@ -1,11 +1,19 @@
-import tour1 from './images/hero1.jpeg';
-import tour2 from './images/hero2.jpeg';
-import tour3 from './images/hero3.jpeg';
-import tour4 from './images/hero4.jpeg';
-import tour5 from './images/hero5.jpeg';
-import tour6 from './images/hero6.jpg';
-import tour7 from './images/hero7.jpg';
-
+import tour1 from './images/hero9.jpg';
+import tour2 from './images/hero10.jpg';
+import tour3 from './images/hero11.jpg';
+import tour4 from './images/hero12.jpg';
+import tour5 from './images/hero13.jpg';
+import tour6 from './images/hero14.jpg';
+import tour7 from './images/hero15.jpg';
+import tour8 from './images/hero8.jpg'
+import tour9 from './images/hero16.jpg'
+import tour10 from './images/hero17.jpg'
+import tour11 from './images/hero18.jpg'
+import tour12 from './images/hero19.jpg'
+import tour13 from './images/hero20.jpg'
+// import tour14 from './images/hero21.jpg'
+import tour15 from './images/hero22.jpg'
+import tour16 from './images/hero23.jpg'
 
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
@@ -130,5 +138,49 @@ export const list = [
   {
     id: 7,
     image: tour7
+  },
+  {
+    id: 8,
+    image: tour8
+  }
+  ,
+  {
+    id: 9,
+    image: tour9
+  }
+  ,
+  {
+    id: 10,
+    image: tour10
+  }
+  ,
+  {
+    id: 11,
+    image: tour11
+  }
+  ,
+  {
+    id: 12,
+    image: tour12
+  }
+  ,
+  {
+    id: 13,
+    image: tour13
+  }
+  ,
+  // {
+  //   id: 14,
+  //   image: tour14
+  // }
+  
+  {
+    id: 14,
+    image: tour15
+  }
+  ,
+  {
+    id: 15,
+    image: tour16
   }
 ];
